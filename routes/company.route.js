d97a91d06ac6448ffcc44ededa22629ff2157d86
Ankit-Mishra2025 <devoankit2025@gmail.com>
2026-03-01 22:01:31 +0530

    Backend code

---
+++ routes/company.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import {
+  getComapnyById,
+  getCompany,
+  registerCompany,
+  updateCompany,
+} from "../controllers/company.controller.js";
+import { singleUpload } from "../middlewares/multer.js";
+const router = express.Router();
+
+// router.route()
+router.route("/register").post(isAuthenticated, registerCompany);
+router.route("/get").get(isAuthenticated, getCompany);
+router.route("/get/:id").get(isAuthenticated, getComapnyById);
+router.route("/update/:id").put(isAuthenticated, singleUpload, updateCompany);
+
+export default router;
